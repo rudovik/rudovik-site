1f1d08d46df64887ea172aee55235bb4d09c199a
rudovik <giguni@ukr.net>
2019-04-05 01:10:19 +0300

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+	constructor (els, offset) {
+		this.itemsToReveal = els;
+		this.offset = offset;
+		this.hideInitially();
+		this.createWaypoints();
+	}
+
+	hideInitially () {
+		this.itemsToReveal.addClass("reveal-item");
+	}
+
+	createWaypoints () {
+		let that = this;
+		this.itemsToReveal.each(function () {
+			let currentItem = this;
+
+			return new Waypoint({
+				element: currentItem,
+				handler: () => {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offset
+			});
+		});
+	}
+}
+
+export default RevealOnScroll;
